33edce07ea2da09e980c2de248f8d2cbdb875581
jegadeeshar <169086931+jegadeeshar@users.noreply.github.com>
2025-11-05 14:53:42 +0530

    Added main.spec.tsx

---
+++ src/modules/enquiry/routes/index.tsx
@@ -0,0 +1,10 @@
+import type { RouteObject } from 'react-router-dom';
+
+const enquiryRoutes: Array<RouteObject> = [
+  {
+    path: 'enquiry',
+    element: <div>Enquiry Module</div>,
+  },
+];
+
+export default enquiryRoutes;
