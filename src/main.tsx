a55450d4c77422d3c56c3036f034e62ae1bf3b4e
jegadeeshar <169086931+jegadeeshar@users.noreply.github.com>
2025-11-05 17:33:53 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import LabelProvider from '@/core/providers/LabelProvider';
 
 const rootElement = document.getElementById('root');
 
@@ -12,7 +13,9 @@ if (!rootElement) {
 createRoot(rootElement).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <LabelProvider>
+        <App />
+      </LabelProvider>
     </BrowserRouter>
   </StrictMode>
 );
