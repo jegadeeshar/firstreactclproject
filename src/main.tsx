748ffdb5b0aee30783fb3bf509b72ae55bfba4ca
jegadeeshar <169086931+jegadeeshar@users.noreply.github.com>
2025-11-06 10:24:16 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,28 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { ErrorBoundary } from 'react-error-boundary';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import customTheme from '@core/theme';
+import App from '@core/containers/App.tsx';
+import ErrorFallback from '@core/containers/ErrorFallback';
 import LabelProvider from '@/core/providers/LabelProvider';
 
 const rootElement = document.getElementById('root');
 
-if (!rootElement) {
-  throw new Error('Root element not found');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <BrowserRouter>
+          <ThemeProvider theme={customTheme}>
+            <LabelProvider>
+              <CssBaseline />
+              <App />
+            </LabelProvider>
+          </ThemeProvider>
+        </BrowserRouter>
+      </ErrorBoundary>
+    </StrictMode>
+  );
 }
-
-createRoot(rootElement).render(
-  <StrictMode>
-    <BrowserRouter>
-      <LabelProvider>
-        <App />
-      </LabelProvider>
-    </BrowserRouter>
-  </StrictMode>
-);
